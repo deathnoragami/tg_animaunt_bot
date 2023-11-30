2b72ab554f159b5fb36648032059353458e31dcc
Антон <doobydouglas90@gmail.com>
2023-12-01 01:01:08 +0300

    its alive

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ from config import REDIS_HOST, REDIS_PORT
 API_PREFIX = '/v1'
 VIDEO_ROOT = 'video'
 # для разарботки
-REDIS_HOST = 'localhost'
+# REDIS_HOST = 'localhost'
 REDIS_BROKER = f'redis://{REDIS_HOST}:{REDIS_PORT}'
 REDIS_BACKEND = f'redis://{REDIS_HOST}:{REDIS_PORT}/1'
